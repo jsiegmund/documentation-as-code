9b91e44c47a4bc01f43db04cdf939ceea890c3cc
Jasper Siegmund <1820644+jsiegmund@users.noreply.github.com>
2020-01-02 14:16:23 +0100

    Updated build script to include plantUML generation

--- build-docs.ps1
+++ build-docs.ps1
@@ -10,9 +10,17 @@
     http://www.github.com/jsiegmund/documentation-as-code
 #>
 
+param (
+  [Switch]$ExcludeImages = $false
+)
+
 # Set variables
 $scriptsPath = $PSScriptRoot
 $pandocExePath = "pandoc.exe"
+$plantumlExePath = "plantuml.exe"
+$diagramsOutputFolder = "$scriptsPath/source"
+
+Write-Host "Converting Markdown *.md files to reStructuredText .rst files." -ForegroundColor Yellow
 
 # Run pandoc for every .md file and convert it in-place to .rst
 Get-ChildItem "source/*.md" | ForEach-Object {
@@ -30,5 +38,28 @@ Get-ChildItem "source/*.md" | ForEach-Object {
   Start-Process $pandocExePath -ArgumentList $pandocArgs -NoNewWindow -Wait
 }
 
+# Image generation might be excluded to optimize build time when diagrams didn't change
+if (-not $ExcludeImages.IsPresent) {
+
+  Write-Host "Generating image files from .puml C4 diagrams." -ForegroundColor Yellow
+
+  # Run plantuml for every .puml file found in the diagrams folder
+  Get-ChildItem "source/diagrams/*.puml" | ForEach-Object {
+    $fileFullName = $_.FullName
+
+    $plantumlArgs = @(    
+      "$fileFullName",
+      "-o $diagramsOutputFolder/images/diagrams"
+    )
+
+    # Starts the actual pandoc process
+    Start-Process $plantumlExePath -ArgumentList $plantumlArgs -NoNewWindow -Wait
+  }
+}
+
+Write-Host "Kicking off Sphinx build process." -ForegroundColor Yellow
+
 # Start the make process which uses Sphynx to convert RST to HTML
 Start-Process "$scriptsPath\make.bat" -ArgumentList "html" -NoNewWindow -Wait
+
+Write-Host "Build process done." -ForegroundColor Green
